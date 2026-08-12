94701b21496624369bfaa04567203b34320bba85
Abhijith Jaideep <abhijithjaideep176@gmail.com>
2026-08-12 23:46:55 +1000

    Make the blog responsive and drop dark mode

--- frontend/src/Components/Navbar/NavBar.jsx
+++ frontend/src/Components/Navbar/NavBar.jsx
@@ -1,14 +1,15 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext, useEffect } from 'react'
 import "./navbar.css"
 import { NavLink, Link } from "react-router-dom"
-import AlertContext from '../../context/Alert/AlertContext'
 import UserContext from '../../context/User/UserContext'
 
+/**
+ * Bootstrap's collapse handles small screens. The bundle is already loaded in
+ * public/index.html, so the toggler needs no extra JavaScript here.
+ *
+ * The dark mode switch that used to sit at the far left has been removed.
+ */
 const NavBar = (props) => {
-
-    const [icon, seticon] = useState("fa-solid fa-moon")
-    const context = useContext(AlertContext)
-    const { showAlert, changeMode, mode } = context
 
     const usercontext = useContext(UserContext)
     const { userdata, fetchuserdata } = usercontext
@@ -19,59 +20,70 @@ const NavBar = (props) => {
         // eslint-disable-next-line
     }, [props.token])
 
-
-
-
     return (
-        <nav className={`navbar navbar-${mode}`} style={{ backgroundColor: mode === "light" ? "darkorange" : "#212529" }} >
+        <nav
+            className="navbar navbar-expand-lg navbar-light"
+            style={{ backgroundColor: "darkorange" }}
+        >
+            <div className="container-fluid">
 
+                {/* Home stays outside the collapse so there is always a way
+                    back on a phone, even with the menu shut. */}
+                <NavLink className="navbar-brand" to="/">
+                    <i className="fa-solid fa-house-chimney"></i> Home
+                </NavLink>
 
-            <div className='nav-links'>
-                <div className='d-flex'>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#mainNav"
+                    aria-controls="mainNav"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
 
+                <div className="collapse navbar-collapse" id="mainNav">
 
-
-                    <div className="form-check form-switch navbar-brand m-0">
-                        <input className="form-check-input" style={{ backgroundColor: mode === "light" ? "white" : "darkorange" }} onClick={() => {
-                            changeMode();
-
-                            if (icon === "fa-solid fa-moon")
-                                seticon("fa-solid fa-sun")
-                            else
-                                seticon("fa-solid fa-moon")
-                            showAlert(`${mode === "light" ? "Enabled" : "Disabled"} dark mode`, "primary")
-                        }} type="checkbox" role="switch" id="flexSwitchCheckDefault" defaultChecked={mode === "dark" ? true : false} />
-
+                    <div className="navbar-nav me-auto">
+                        {props.token && <>
+                            <Link className="nav-link" to="/write">
+                                <i className="fa-solid fa-feather"></i><span> Write</span>
+                            </Link>
+                            <Link className="nav-link" to="/feed">
+                                <i className="fa-solid fa-book-open"></i><span> Feed</span>
+                            </Link>
+                        </>}
                     </div>
-                    <div className="navbar-brand">
-                        <i style={{ color: mode === "dark" ? "darkorange" : "black" }} className={`${icon}`}></i>
+
+                    <div className="navbar-nav ms-auto align-items-lg-center">
+                        {!props.token && <>
+                            <Link className="nav-link" to="/signup">
+                                <i className="fa-solid fa-user-plus"></i> Signup
+                            </Link>
+                            <Link className="nav-link" to="/login">
+                                <i className="fa-solid fa-user-pen"></i> Login
+                            </Link>
+                        </>}
+
+                        {props.token &&
+                            <Link to="/profile" className="nav-link d-flex align-items-center gap-2">
+                                {userdata.profilepic &&
+                                    <img
+                                        src={`data:image/jpeg;base64,${userdata.profilepic}`}
+                                        alt="profilepic"
+                                        style={{ borderRadius: "50%", height: "36px", width: "36px", border: "2px solid black" }}
+                                    />}
+                                {!userdata.profilepic && <i className="fa-solid fa-circle-user"></i>}
+                                <span>{userdata.username}</span>
+                            </Link>
+                        }
                     </div>
 
-                    <NavLink className="navbar-brand" to="/"><i className="fa-solid fa-house-chimney"></i> Home</NavLink>
-                    {props.token &&
-                        <div className='d-flex'>
-                            <Link className="navbar-brand" to="/write"><i className="fa-solid fa-feather"></i><span> Write</span></Link>
-                            <Link className="navbar-brand" to="/feed" ><i className="fa-solid fa-book-open"></i><span> Feed</span></Link>
-                        </div>
-                    }
-                </div>
-
-                <div className='d-flex'>
-
-                    {!props.token && <>
-                        <Link className="navbar-brand" to="/signup"><i className="fa-solid fa-user-plus"></i> Signup</Link>
-                        <Link className="navbar-brand" to="/login"><i className="fa-solid fa-user-pen"></i> Login</Link>
-                    </>}
-
-                    {props.token && <Link to="/profile" className='navbar-brand'>
-                            {userdata.profilepic && <img src={`data:image/jpeg;base64,${userdata.profilepic}`} alt="profilepic" style={{ borderRadius: "50%", height: "36px", width: "36px", border: "2px solid black" }} />}
-                            {!userdata.profilepic && <i className="fa-solid fa-circle-user"></i>}
-                            <span style={{ margin: "10%" }}>{userdata.username}</span>
-                    </Link>
-                    }
                 </div>
             </div>
-
         </nav>
     )
 }
